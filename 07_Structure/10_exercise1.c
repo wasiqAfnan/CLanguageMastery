c453a10ca922a0cd6d7bc4716fa8fe0bd04f94ab
afnan <wansari964@yahoo.com>
2024-07-09 14:37:43 +0530

    Added exercise in structure

---
+++ 07_Structure/10_exercise1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*Create a structure named Book to store book details like title, author, and price. 
+Write a C program to input details for three books, find the most expensive and 
+the lowest priced books, and display their information.
+*/
+
+void main(){
+    
+    return 0;
+}
---
+++ 07_Structure/11_exercise2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+Create a structure named Complex to represent a complex number with real 
+and imaginary parts. Write a C program to add and multiply two complex numbers.
+*/
+
+void main(){
+    
+    return 0;
+}
